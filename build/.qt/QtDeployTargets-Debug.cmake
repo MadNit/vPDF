9f4d5a80ff95193213c0b350297c8796a95293b8
MadNit <53405149+MadNit@users.noreply.github.com>
2026-03-17 08:50:13 +0530

    chore: remove build artifacts and update .gitignore

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_pdfkill_FILE C:/projects/meic/pdfkill/build/Debug/pdfkill.exe)
-set(__QT_DEPLOY_TARGET_pdfkill_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_pdfkill_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/projects/meic/pdfkill/third_party/pdfium/bin/pdfium.dll;C:/projects/meic/pdfkill/build/vcpkg_installed/x64-windows/debug/bin/qpdf30.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
